e0e53734a5b7688a6eaf2df22266995a81feb548
Nathaniel Xu <xunathan@cs.ubc.ca>
2024-04-01 23:39:15 -0700

    add imagenet-c

--- src/data/imagenet_c/imagenet_c.py
+++ src/data/imagenet_c/imagenet_c.py
@@ -1,5 +1,6 @@
+import numpy as np
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, Tuple
 from torchvision.datasets import ImageFolder
 __all__=['ImageNetC']
 
@@ -13,3 +14,11 @@ class ImageNetC(ImageFolder):
         super().__init__(Path(root)/'data'/split, transform, target_transform)
 
 
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        path, target = self.samples[index]  # (str, int)
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return sample, np.array(target, dtype=np.float32)[np.newaxis]
