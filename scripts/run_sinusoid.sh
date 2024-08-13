bc703706879d83eac8716b87bac9db9d0db70964
Nathaniel Xu <xunathan@cedar1.int.cedar.computecanada.ca>
2024-08-13 13:34:15 -0700

    sinusoid script changes

--- scripts/run_sinusoid.sh
+++ scripts/run_sinusoid.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-dsuth
 #SBATCH --gpus-per-node=1       # Request 1 available GPU (--gpus-per-node=p100:1)
 #SBATCH --mem=4000M             # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=0-12:00:00       # DD-HH:MM:SS
+#SBATCH --time=0-24:00:00       # DD-HH:MM:SS
 #SBATCH --job-name=sinusoid
 #SBATCH --output=logs/%x/slurm-%j.out   # output file. %x is the job name, %N is the hostname, %j is the job id
 
@@ -12,8 +12,8 @@ SOURCE_DIR=$PROJ_DIR/src
 SCRIPT_DIR=$PROJ_DIR/scripts
 cd $SCRIPT_DIR
 
-module load python/3.10 scipy-stack cuda cudnn
 source $VENV_DIR/bin/activate
+module load python/3.10 scipy-stack cuda cudnn
 
 # config files
 train_root=config/exp/train/
@@ -134,21 +134,32 @@ function eval_args {
     esac
 }
 
-# run=1
-# datasets="sinusoid.1000 sinusoid.2000 sinusoid.3000 sinusoid.4000"
+run=3
+datasets="sinusoid.1000 sinusoid.2000 sinusoid.4000 sinusoid.3000"
+
 # source train.sh $run "hsic hsic-tied bandwidth" "$datasets"
+# source eval.sh $run "hsic hsic-tied bandwidth" "$datasets"
+# dataset_to_testsize["sinusoid.4000"]="50 100 200 500 1000 2000"
 # source eval.sh $run "hsic hsic-tied bandwidth" "$datasets"
 
 # source train.sh $run "c2st" "$datasets"
 # source eval.sh $run "c2st-l c2st-s" "$datasets"
+# dataset_to_testsize["sinusoid.4000"]="50 100 200 500 1000 2000"
+# source eval.sh $run "c2st-l c2st-s" "$datasets"
 
 # source train.sh $run "mmd" "$datasets"
+# source eval.sh $run "mmd" "$datasets"
+# dataset_to_testsize["sinusoid.4000"]="50 100 200 500 1000 2000"
 # source eval.sh $run "mmd" "$datasets"
 
 # source train.sh $run "infonce" "$datasets"
 # source eval.sh $run "infonce" "$datasets"
+# dataset_to_testsize["sinusoid.4000"]="50 100 200 500 1000 2000"
+# source eval.sh $run "infonce" "$datasets"
 
 # source train.sh $run "nwj" "$datasets"
+# source eval.sh $run "nwj" "$datasets"
+# dataset_to_testsize["sinusoid.4000"]="50 100 200 500 1000 2000"
 # source eval.sh $run "nwj" "$datasets"
 
 
